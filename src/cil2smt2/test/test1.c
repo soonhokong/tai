d9ec82dc5a64f53ab8d562d2e07a299761fd73df
Soonho Kong <soonhok@cs.cmu.edu>
2014-04-23 16:49:42 -0400

    tests(cil2smt2): add more tests

---
+++ src/cil2smt2/test/test1.c
@@ -0,0 +1,10 @@
+double f (double x) {
+    double y;
+    y = 2.0;
+    if ((x - 0.2) < 0.8) {
+        y = x;
+    } else {
+        y = 3.0;
+    }
+    return y;
+}
---
+++ src/cil2smt2/test/test2.c
@@ -0,0 +1,9 @@
+double abs(double x) {
+    double ret;
+    if (x > 0.0) {
+        ret = x;
+    } else {
+        ret = - x;
+    }
+    return ret;
+}
