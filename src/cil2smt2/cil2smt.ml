72edb8b76f0e74a6e4fe312e6c406ef7aa6a8e08
Wei Chen <weichen1@andrew.cmu.edu>
2014-04-24 19:52:54 -0400

    fix(cil2smt.ml): correct variable numbering

--- src/cil2smt2/cil2smt.ml
+++ src/cil2smt2/cil2smt.ml
@@ -90,62 +90,100 @@ let rec translation file_name=
 
 and translate_function f : expr list =
   match f with
-  | GFun (fd, _) -> translate_blocks [fd.sbody]
+  | GFun (fd, _) ->
+    let f, vc = translate_blocks [fd.sbody] Vcmap.empty in
+    f
   | _ -> failwith "should be function"
 
-and translate_blocks blocks : expr list =
-  let translate_block block =
-    translate_stmts block.bstmts
+and translate_blocks blocks (vc : Vcmap.t): expr list * Vcmap.t =
+  let translate_block block vc =
+    translate_stmts block.bstmts vc
   in
-  List.flatten (List.map translate_block blocks)
+  List.fold_left
+    (fun accu b ->
+       let (bs, vc) = accu in
+       let stmts1, vc1 = translate_stmts b.bstmts vc in
+       (bs @ stmts1, vc1)
+    )
+    ([], vc) blocks
 
-and translate_stmts stmts : expr list =
-  let translate_stmt stmt =
-    translate_stmtkinds [stmt.skind]
+and translate_stmts stmts (vc : Vcmap.t) : expr list * Vcmap.t =
+  let translate_stmt stmt (vc : Vcmap.t) : expr list * Vcmap.t =
+    translate_stmtkinds [stmt.skind] vc
   in
-  List.flatten (List.map translate_stmt stmts)
+  List.fold_left
+    (fun accu s ->
+       let (ss, vc) = accu in
+       let (ss1, vc1) = translate_stmtkinds [s.skind] vc in
+       (ss @ ss1, vc1)
+    )
+    ([], vc) stmts
 
-and translate_stmtkinds skinds : expr list =
-  let translate_stmtkind sk =
+and mk_var s i = s ^ (string_of_int i)
+
+and gen_copy_formula diff_list =
+  Basic.make_and
+    (List.map
+       (fun (s, a, b) ->
+          Basic.Eq ((Basic.Var (mk_var s a)), (Basic.Var (mk_var s b)))
+       )
+       diff_list)
+
+and translate_stmtkinds skinds (vc : Vcmap.t) : expr list * Vcmap.t =
+  let translate_stmtkind sk vc : expr list * Vcmap.t =
     match sk with
     | Instr ins ->
-      translate_instrs ins
+      translate_instrs ins vc
     | Return _ -> failwith "not now return"
     | Goto _ -> failwith "not now goto"
     | Break _ -> failwith "not now break"
     | Continue _ -> failwith "not now continue"
     | If (be, e1, e2, _)->
-      let be' = translate_exps [be] in
-      let e1' = translate_blocks [e1] in
-      let e2' = translate_blocks [e2] in
+      let be', vc0 = translate_exps [be] vc in
+      let e1', vc1 = translate_blocks [e1] vc0 in
+      let e2', vc2 = translate_blocks [e2] vc0 in
       begin
       match (all is_formula be'), (all is_formula (e1' @ e2')) with
       | true, true ->
          let F be = List.hd be' in
          let es1 = List.map extract_formula e1' in
          let es2 = List.map extract_formula e2' in
+
+         let vc3 = join vc1 vc2 in
+         let diff_conseq = diff vc1 vc3 in
+         let diff_alter = diff vc2 vc3 in
+         let copy_formula_conseq = gen_copy_formula diff_conseq in
+         let copy_formula_alter = gen_copy_formula diff_alter in
          [F (
              Basic.make_and
-               [ Basic.make_or [be; Basic.make_and es1]; Basic.make_or [Basic.Not be; Basic.make_and es2]]
-           )]
+               [ Basic.Imply (be, Basic.make_and (es1 @ [copy_formula_conseq]));
+                 Basic.Imply (Basic.Not be, Basic.make_and (es2 @ [copy_formula_alter]))]
+           )], vc3
       | _ -> failwith "should all be formula"
       end
     | Switch _ -> failwith "not now switch"
     | Loop _ -> failwith "not now loop"
     | Block b ->
-       translate_blocks [b]
+       translate_blocks [b] vc
     | TryFinally _ -> failwith "not now try"
     | TryExcept _ -> failwith "not now try"
     | ComputedGoto _ -> failwith "not now comput goto"
   in
-  List.flatten (List.map translate_stmtkind skinds)
+  List.fold_left
+    (fun accu b ->
+       let (prev, vc) = accu in
+       let ss, vc1 = translate_stmtkind b vc in
+       (prev @ ss, vc1)
+    )
+    ([], vc) skinds
 
-and translate_exps exps : expr list =
-  let translate_exp (e : Cil.exp) =
+and translate_exps exps (vc : Vcmap.t) : expr list * Vcmap.t =
+  let translate_exp (e : Cil.exp) (vc : Vcmap.t) : expr * Vcmap.t =
     match e with
-    | Const c -> E (translate_const c)
+    | Const c -> E (translate_const c), vc
     | Lval lval ->
-       E (translate_lval lval)
+      let e', vc1 = translate_lval lval vc in
+      E e', vc1
     | SizeOf _ -> failwith "not now sizeof"
     | SizeOfE _ -> failwith "not now sizeof"
     | SizeOfStr _ -> failwith "not now sizeof"
@@ -154,12 +192,13 @@ and translate_exps exps : expr list =
     | AddrOfLabel _ -> failwith "not now align"
     | UnOp (uop, e, _) ->
       begin
-        let e' = List.hd (translate_exps [e]) in
+        let es, vc1 = translate_exps [e] vc in
+        let e' = List.hd es in
         match uop with
         | Neg ->
           begin
             match e' with
-            | E e'' -> E (Basic.Neg e'')
+            | E e'' -> E (Basic.Neg e''), vc1
             | _ -> failwith "should be an expression"
           end
         | BNot -> failwith "todo"
@@ -167,13 +206,16 @@ and translate_exps exps : expr list =
           begin
             match e' with
             | E e'' -> failwith "should be a formula"
-            | F e'' -> F (Basic.Not e'')
+            | F e'' -> F (Basic.Not e''), vc1
           end
       end
     | BinOp (bop, e1, e2, _) ->
-        begin
-          let e1' = List.hd (translate_exps [e1]) in
-          let e2' = List.hd (translate_exps [e2]) in
+      begin
+        let es1, vc1 = translate_exps [e1] vc in
+        let es2, vc2 = translate_exps [e2] vc in
+        let e1' = List.hd es1 in
+        let e2' = List.hd es2 in
+        let vc3 = join vc1 vc2 in
           match bop with
           | PlusA ->
              begin
@@ -181,7 +223,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E e1' = e1' in
                 let E e2' = e2' in
-                E (Basic.Add [e1'; e2'])
+                E (Basic.Add [e1'; e2']), vc3
               | false -> failwith "not all expression"
             end
           | PlusPI  -> failwith "not now plus"
@@ -192,7 +234,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E e1' = e1' in
                 let E e2' = e2' in
-                E (Basic.Sub [e1'; e2'])
+                E (Basic.Sub [e1'; e2']), vc3
               | false -> failwith "not all expression"
             end
           | MinusPI -> failwith "not now minus"
@@ -203,7 +245,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E e1' = e1' in
                 let E e2' = e2' in
-                E (Basic.Mul [e1'; e2'])
+                E (Basic.Mul [e1'; e2']), vc3
               | false -> failwith "not all expression"
             end
           | Div ->
@@ -212,7 +254,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E e1' = e1' in
                 let E e2' = e2' in
-                E (Basic.Div (e1', e2'))
+                E (Basic.Div (e1', e2')), vc3
               | false -> failwith "not all expression"
             end
           | Mod -> failwith "not now mod"
@@ -224,7 +266,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E f1 = e1' in
                 let E f2 = e2' in
-                F (Basic.Lt (f1, f2))
+                F (Basic.Lt (f1, f2)), vc3
               | _ -> failwith "not all formula"
             end
           | Gt ->
@@ -233,7 +275,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E f1 = e1' in
                 let E f2 = e2' in
-                F (Basic.Gt (f1, f2))
+                F (Basic.Gt (f1, f2)), vc3
               | _ -> failwith "not all formula"
             end
           | Le ->
@@ -242,7 +284,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E f1 = e1' in
                 let E f2 = e2' in
-                F (Basic.Le (f1, f2))
+                F (Basic.Le (f1, f2)), vc3
               | _ -> failwith "not all formula"
             end
           | Ge ->
@@ -251,7 +293,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E f1 = e1' in
                 let E f2 = e2' in
-                F (Basic.Ge (f1, f2))
+                F (Basic.Ge (f1, f2)), vc3
               | _ -> failwith "not all formula"
             end
           | Eq ->
@@ -260,7 +302,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E f1 = e1' in
                 let E f2 = e2' in
-                F (Basic.Eq (f1, f2))
+                F (Basic.Eq (f1, f2)), vc3
               | _ -> failwith "not all formula"
             end
           | Ne ->
@@ -269,7 +311,7 @@ and translate_exps exps : expr list =
               | true ->
                 let E f1 = e1' in
                 let E f2 = e2' in
-                F (Basic.Eq (f1, f2))
+                F (Basic.Eq (f1, f2)), vc3
               | _ -> failwith "not all formula"
             end
           | BAnd -> failwith ""
@@ -281,7 +323,7 @@ and translate_exps exps : expr list =
               | true ->
                 let F f1 = e1' in
                 let F f2 = e2' in
-                F (Basic.And [f1; f2])
+                F (Basic.And [f1; f2]), vc3
               | _ -> failwith "not all formula"
             end
           | LOr ->
@@ -290,46 +332,71 @@ and translate_exps exps : expr list =
               | true ->
                 let F f1 = e1' in
                 let F f2 = e2' in
-                F (Basic.Or [f1; f2])
+                F (Basic.Or [f1; f2]), vc3
               | _ -> failwith "not all formula"
             end
         end
     | CastE (_, e) ->
       begin
-        let e = translate_exps [e] in
+        let e, vc1 = translate_exps [e] vc in
         match all is_exp e with
         | true ->
-          List.hd e
+          List.hd e, vc1
         | false -> failwith "not a expression for caste"
       end
     | AddrOf _ -> failwith "not now addrOf"
     | StartOf _ -> failwith "not now startOf"
     | Question _ -> failwith "not now question"
   in
-  List.map translate_exp exps
+  List.fold_left
+    (fun accu b ->
+       let (prev, vc) = accu in
+       let ss, vc1 = translate_exp b vc in
+       (prev @ [ss], vc1)
+    )
+    ([], vc) exps
 
 
-and translate_instrs ins : expr list =
-  let translate_inst ins =
+and translate_instrs ins (vc : Vcmap.t) : expr list * Vcmap.t =
+  let translate_inst ins (vc : Vcmap.t) : expr * Vcmap.t =
     match ins with
     | Set (lval, e, _) ->
-      let lval = translate_lval lval in
-      let e' = List.hd (translate_exps [e]) in
+      let exps, vc1 = translate_exps [e] vc in
+      let e' = List.hd exps in
+      let s = extract_var_name lval in
+      let vc2 = update s vc1 in
+      let lval, vc3 = translate_lval lval vc2 in
       begin
         match e' with
-        | E e' -> F (Basic.Eq (lval, e'))
+        | E e' -> F (Basic.Eq (lval, e')), vc3
         | _ -> failwith "should be an expression"
       end
     | Call _ -> failwith "not now call"
     | Asm _ -> failwith "not now asm"
   in
-  List.map translate_inst ins
+  List.fold_left
+    (fun accu b ->
+       let (prev, vc) = accu in
+       let ss, vc1 = translate_inst b vc in
+       (prev @ [ss], vc1)
+    )
+    ([], vc) ins
 
-and translate_lval l : Basic.exp =
+and translate_lval l (vc : Vcmap.t) : Basic.exp * Vcmap.t =
   let (lhost, _) = l in
   match lhost with
-  | Var vi -> Basic.Var (vi.vname)
+  | Var vi ->
+    let c, vc1 = lookup vi.vname vc in
+    Basic.Var (vi.vname ^ (string_of_int c)), vc1
   | _ -> failwith "not now (translate_lval)"
+
+
+and extract_var_name l  =
+  let (lhost, _) = l in
+  match lhost with
+  | Var vi ->
+    vi.vname
+  | _ -> failwith "error var name"
 
 and translate_const (c : Cil.constant) =
   match c with
--- src/cil2smt2/test/test1.c
+++ src/cil2smt2/test/test1.c
@@ -1,10 +1,11 @@
 double f (double x) {
     double y;
     y = 2.0;
+    y = 3.0;
     if ((x - 0.2) < 0.8) {
         y = x;
-    } else {
-        y = 3.0;
     }
+    double z;
+    z = y;
     return y;
 }
