5974d604c99124b8a8993d7d8e7d10199a7cb055
Soonho Kong <soonhok@cs.cmu.edu>
2014-04-25 01:48:44 -0400

    test(cil2smt2): update test7.c

--- src/cil2smt2/test/test7.c
+++ src/cil2smt2/test/test7.c
@@ -1,7 +1,21 @@
-double msin(double x) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+double f(double k) {
     double array[2] = {4.0, 5.0};
     int x = 0;
     int y = 1;
-    double tmp = array[x] + array[y];
-    return tmp;
+    double tmp1 = array[x];
+    double tmp2 = array[y];
+    double ret = tmp1 + tmp2;
+    return k + ret;
+}
+
+int main (int argc, char *argv[])
+{
+    assert(argc == 2);
+    double arg = atof(argv[1]);
+    printf("%g\n", f(arg));
+    return 0;
 }
