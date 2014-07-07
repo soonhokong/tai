9574c810d26b5c4ebe0399fdfe3cf4f8f55de41d
Soonho Kong <soonhok@cs.cmu.edu>
2014-07-07 04:40:36 -0400

    update main loop

--- src/main.py
+++ src/main.py
@@ -15,9 +15,13 @@ __status__ = "Production"
 import argparse
 import logging
 import os
+import math
 
-from util import check_file_or_die, to_interval, read_model, print_model, get_filename, run_test, generate_SMT, run_dReal, is_SAT
+from util import test, check_file_or_die, to_interval, read_model, print_model, get_filename, run_test, generate_SMT, run_dReal, is_SAT
 
+# =================
+# Argument Parsing
+# =================
 parser = argparse.ArgumentParser(description='Test and Infer.')
 parser.add_argument('-l', '--lb',     type=float, help='lower bound', required=True)
 parser.add_argument('-u', '--ub',     type=float, help='upper bound', required=True)
@@ -27,8 +31,6 @@ parser.add_argument('-v', '--verbose',            help='verbose', action='store_
 parser.add_argument('-d', '--debug',              help='debug',   action='store_true', default=False)
 parser.add_argument('srcfile', type=str, help='src file')
 parser.add_argument('binfile', type=str, help='bin file')
-
-
 args = parser.parse_args()
 (lb, ub, prec, r, srcfile, binfile) = (args.lb, args.ub, args.prec, args.radius, args.srcfile, args.binfile)
 
@@ -36,10 +38,8 @@ args = parser.parse_args()
 # Logging
 # =================
 if args.verbose:
-    print "VERBOSE"
     logging.basicConfig(format='%(asctime)s --- %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 elif args.debug:
-    print "DEBUG"
     logging.basicConfig(format='%(asctime)s --- %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 else:
     logging.basicConfig(format='%(asctime)s --- %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
@@ -49,28 +49,33 @@ else:
 # =================
 tai='/Users/soonhok/work/tai/src/cil2smt2/main.native'
 dreal='/Users/soonhok/bin/dReal'
-
 for filename in [tai, dreal, srcfile, binfile]:
     check_file_or_die(filename)
 
+# =================
+# Start Main Loop
+# =================
 c = args.lb
-d = c + r
+d = min(c + r, args.ub)
+eps = 10000.001
+base = os.path.basename(binfile)
 
-while c <= ub:
-    print "[C, D] =", to_interval(c, d)
+while c < ub:
+    logging.info("Checking a range [%f, %f] with precision %f" % (c, d, prec))
+    smt2_filename = get_filename([base, 'formula', c, d, prec], ext = 'smt2')
+    smt2_result_filename = get_filename([base, 'dreal_output', c, d, prec])
+    smt2_model_filename = get_filename([base, 'formula', c, d, prec], ext = 'smt2.model')
 
-    output_filename = get_filename('output', c, d, prec)
-    info_filename = get_filename('info', c, d, prec)
-    smt2_filename = get_filename('formula', c, d, prec, ext = 'smt2')
-    smt2_result_filename = get_filename('dreal_output', c, d, prec)
-    smt2_model_filename = get_filename('formula', c, d, prec, ext = 'smt2.model')
-
-    output_file = open(output_filename, 'w')
-    info_file  = open(info_filename, 'w')
     smt2_file  = open(smt2_filename, 'w')
     smt2_result_file = open(smt2_result_filename, 'w')
 
-    test_out = run_test(binfile, c, output_file, info_file)
+    (test_out, v_p, v_o, info_filename) = test(binfile, math.sin, c, eps)
+    print "INFO:", info_filename
+
+    if test_out == False:
+        logging.error("Test on %f is not passed." % c)
+        exit(1)
+
     smt2_out = generate_SMT(tai, c, d, info_filename, srcfile, smt2_file)
     dreal_out = run_dReal(dreal, smt2_filename, prec, smt2_result_file)
 
@@ -81,7 +86,19 @@ while c <= ub:
         logging.info("SAT: " + to_interval(c, d))
         model = read_model(smt2_model_filename)
         print_model(model)
-        test_out = run_test(binfile, model['x0'][0], output_file, info_file)
+        c_lb = model['x0'][0]
+        c_ub = model['x0'][1]
+        c_mid = c_lb / 2.0 + c_ub / 2.0
+        test_out = test(binfile, math.sin, c, eps)
+        if test_out == 0:
+            # Test is passed. 'c_' is a spurious counterexample
+            logging.info("Test on %f is passed. %f is a spurious counterexample." % (c_mid, c_mid))
+            logging.info("Use smaller precision value: prec = %f." % prec)
+            prec = prec / 2.0
+        else:
+            # Test is not passed. 'c_' was a real counterexample
+            logging.info("Test on %f is not passed. %f is a real counterexample." % (c_mid, c_mid))
+            d = c_lb
         # run the binary with x', check whether it's a false positive or not
         # if (false positive):
         #     run dReal with higher precision
@@ -92,8 +109,9 @@ while c <= ub:
         #     and check [c, x']
     else:
         # UNSAT Case
-        print "UNSAT:", to_interval(c, d)
+        logging.info("UNSAT: " + to_interval(c, d))
         c = d
         d = c + r
+        prec = args.prec
 
-    break;
+    logging.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
--- src/util.py
+++ src/util.py
@@ -16,26 +16,86 @@ import logging
 import os.path
 import re
 import subprocess
+import mpmath
 
-def run_test(binfile, c, output_file, info_file):
+def get_filename(l, ext=None, dir='tmp/'):
+    ret = dir + str.join("_", map(str, l))
+    if ext:
+        ret = ret + "." + ext
+    return ret
+
+def exec_file(filename, x):
+    """
+    Execute file ``filename`` with an argument ``x``.
+    Return its output and created info file
+    """
+    base = os.path.basename(filename)
+    info_filename = get_filename([base, "info", x])
+    info_file  = open(info_filename, 'w')
+    logging.info("Test %s at %f (save info at %s)" % (filename, x, info_filename))
+    returncode = 0
+    try:
+        args = [filename, x]
+        output_bytes = subprocess.check_output(map(str, args), stderr = info_file)
+    except subprocess.CalledProcessError as e:
+        returncode = e.returncode
+    output = float.fromhex(output_bytes)
+
+    logging.info("Test %s at %f, OUTPUT = %s" % (filename, x, output))
+    logging.info("Test %s at %f, Result = %d" % (filename, x, returncode))
+    info_file.close()
+    return (output, info_filename)
+
+def test(filename, oracle, x, eps):
+    v_p, info = exec_file(filename, x)
+    v_o = oracle(x)
+    err = abs(v_o - v_p)
+    logging.info("Test %s at %f" % (filename, x))
+    logging.info("\t Oracle  output = %.15f" % v_o)
+    logging.info("\t Program output = %.15f" % v_p)
+    logging.info("\t Error          = %.15f" % err)
+    logging.info("\t eps            = %.15f" % eps)
+    if err > eps:
+        return (False, v_p, v_o, info)
+    else:
+        return (True, v_p, v_o, info)
+
+def run_test(binfile, c, info_file):
     logging.info("Test %s at %f" % (binfile, c))
     returncode = 0
     try:
-        output = subprocess.check_call([binfile, str(c)], stdout = output_file, stderr = info_file)
+        args = [binfile, c]
+        output_bytes = subprocess.check_output(map(str, args), stderr = info_file)
     except subprocess.CalledProcessError as e:
         returncode = e.returncode
+    output = float.fromhex(output_bytes)
+
+    logging.info("Test %s at %f, OUTPUT = %s" % (binfile, c, output))
     logging.info("Test %s at %f, Result = %d" % (binfile, c, returncode))
     return returncode
 
 def generate_SMT(tai, lb, ub, info_filename, srcfile, smt2_file):
     logging.info("Generate SMT2 for a range [%f, %f] info: %s, src: %s"
                                % (lb, ub, info_filename, srcfile))
-    output = subprocess.check_call([tai, "-l", str(lb), "-u", str(ub), "-i", info_filename, srcfile], stdout = smt2_file)
+    args = [tai,
+            "-l", lb,
+            "-u", ub,
+            "-i", info_filename,
+            srcfile]
+    output = subprocess.check_call(map(str, args), stdout = smt2_file)
     return output
 
 def run_dReal(dreal, smt2_filename, prec, smt2_result_file):
     logging.info("Run dReal: %s" % smt2_filename)
-    output = subprocess.check_call([dreal, "--precision", str(prec), "--model", smt2_filename], stdout = smt2_result_file)
+    args = [dreal,
+            "--precision", prec,
+            "--model",
+            smt2_filename]
+    try:
+        output = subprocess.check_call(map(str, args), stdout = smt2_result_file)
+    except subprocess.CalledProcessError as e:
+        print str.join(" ", map(str, args))
+        exit(1)
     return output
 
 def is_SAT(filename):
@@ -67,9 +127,3 @@ def print_model(model):
     for key, value in model.iteritems():
         logging.info("\t %s = [%f, %f]" % (key, value[0], value[1]))
     logging.info("===================================================================")
-
-def get_filename(s, lb, ub, prec, ext=None, dir='tmp/'):
-    ret = dir + '{0}_{1}_{2}_{3}'.format(s, str(lb), str(ub), str(prec))
-    if ext:
-        ret = ret + "." + ext
-    return ret
